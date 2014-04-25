af80151e2d82ab0ed53fa82572d70067208c21b5
nealkruis <neal.kruis@bigladdersoftware>
2014-04-25 10:30:59 -0600

    Move BESTEST directory to root.

--- test/BESTEST/bin/run.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-params compose ../../../../templates/template.params -f "../../test.txt;../case.txt;soln.txt" -o instance.yaml
-../../../../build/kiva instance.yaml ../*.epw Timeseries.csv
