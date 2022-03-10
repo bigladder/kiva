ded7da92e881e05f8fd1633d2bb45d9249323736
galanca <cesar.galan@colorado.edu>
2022-03-10 09:44:49 -0700

    Revise Tz and Tr initialize variable.

--- src/libkiva/Aggregator.cpp
+++ src/libkiva/Aggregator.cpp
@@ -58,7 +58,7 @@ void Aggregator::calc_weighted_results() {
     validate();
   }
   results.reset();
-  double Tz{NULL}, Tr{NULL};
+  double Tz{-1}, Tr{-1};
   for (auto &instance : instances) {
     Ground *grnd = instance.first;
     Tz = surface_type == Surface::ST_WALL_INT ? grnd->bcs.wallConvectiveTemp
