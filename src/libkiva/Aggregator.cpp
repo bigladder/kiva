7be2d4c62efc79e733dfbd1253f565c370a37676
galanca <galanc3.3@gmail.com>
2022-03-16 08:20:07 -0600

    Fix reoder warning.

--- src/libkiva/Aggregator.cpp
+++ src/libkiva/Aggregator.cpp
@@ -58,7 +58,7 @@ void Aggregator::calc_weighted_results() {
     validate();
   }
   results.reset();
-  double Tz{-1}, Tr{-1};
+  double Tz{293.15}, Tr{293.15};
   for (auto &instance : instances) {
     Ground *grnd = instance.first;
     Tz = surface_type == Surface::ST_WALL_INT ? grnd->bcs.wallConvectiveTemp
--- src/libkiva/Foundation.cpp
+++ src/libkiva/Foundation.cpp
@@ -102,14 +102,14 @@ bool Ranges::isType(double position, RangeType::Type type) {
 
 Foundation::Foundation()
     : deepGroundDepth(40.0), farFieldWidth(40.0), foundationDepth(0.0), orientation(0.0),
-      deepGroundBoundary(DGB_ZERO_FLUX), wallTopBoundary(WTB_ZERO_FLUX),
-      soil(Material(1.73, 1842, 419)), grade(SurfaceProperties(0.9, 0.9, 0.03)),
+      deepGroundBoundary(DGB_ZERO_FLUX), wallTopInteriorTemperature{0}, wallTopExteriorTemperature{0},
+      wallTopBoundary(WTB_ZERO_FLUX), soil(Material(1.73, 1842, 419)), grade(SurfaceProperties(0.9, 0.9, 0.03)),
       coordinateSystem(CS_CARTESIAN), numberOfDimensions(2), useSymmetry(true),
-      reductionStrategy(RS_BOUNDARY), isXSymm{false}, exposedFraction(1.0), useDetailedExposedPerimeter(false),
-      buildingHeight(0.0), hasWall(true), wallTopInteriorTemperature{0}, wallTopExteriorTemperature{0},
-      twoParameters{false}, reductionLength1{0}, reductionLength2{0},
-      linearAreaMultiplier{0}, isYSymm{false}, hasSlab(true), perimeterSurfaceWidth(0.0),
-      hasPerimeterSurface(false), mesh(Mesh()), numericalScheme(NS_ADI),
+      reductionStrategy(RS_BOUNDARY), twoParameters{false}, reductionLength1{0}, reductionLength2{0},
+      linearAreaMultiplier{0}, isXSymm{false}, isYSymm{false}, exposedFraction(1.0),
+      useDetailedExposedPerimeter(false), buildingHeight(0.0),
+      hasWall(true), hasSlab(true), perimeterSurfaceWidth(0.0),
+      hasPerimeterSurface(false),  mesh(Mesh()), numericalScheme(NS_ADI),
       fADI(0.00001), tolerance(1.0e-6), maxIterations(100000), netArea{0}, netPerimeter{0} {}
 
 void Foundation::createMeshData() {
